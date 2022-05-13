e49fb322b9696772e47f1fc66d3a5c5b42aab509
brandonruizmora <brandon_ruizmor@outlook.es>
2022-05-13 13:31:31 -0500

    Added new Model and table to DB

---
+++ prisma/migrations/20220513180612_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Mission" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "lang" VARCHAR(255) NOT NULL,
+    "missionCommander" VARCHAR(255) NOT NULL,
+    "enrollments" INTEGER NOT NULL,
+    "hasCertification" BOOLEAN NOT NULL DEFAULT false,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Mission_pkey" PRIMARY KEY ("id")
+);
